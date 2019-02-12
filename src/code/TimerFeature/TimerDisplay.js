37a592eb03a607e8cdd2065e82dba114d868ea14
Huxley_Millard <gerogiamillardccm@gmail.com>
2019-02-12 17:57:02 +0000

    have a working timer

--- src/code/TimerFeature/TimerDisplay.js
+++ src/code/TimerFeature/TimerDisplay.js
@@ -7,28 +7,39 @@ class TimerDisplay extends Component {
     constructor(props){
         super()
         this.state = props.state
+  
     }
   
-    readable_time = (difference) => {
-        let hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        let minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
-        let seconds = Math.floor((difference % (1000 * 60)) / 1000);
-        return `${hours} : ${minutes} : ${seconds}`;
+    readable_time = () => {
+        if(this.state.currentTime){
+            let difference = this.state.currentTime
+            console.log(difference)
+            let hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+            let minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+            let seconds = Math.floor((difference % (1000 * 60)) / 1000);
+            return `${hours} : ${minutes} : ${seconds}`;
+        } else {
+            return ` one moment `
+        }
     };
 
-    tick(){
-        var _ = setInterval(() => {     
-            this.find_difference(new Date().getTime(), this.state.till);
-        },1000)
+    tick(){        
+        let start =  new Date().getTime();
+        let end =  this.state.till;
+        let currentTime = end - start;
+        this.setState({ currentTime: currentTime })
     }
 
-    find_difference(start, end){
-        let diff = this.state.till - this.state.from;
-        this.readable_time(diff);  
+    componentDidMount(){
+        this.timer = setInterval( ()=> this.tick(), 1000 );   
+    }
+
+    componentWillUnmount(){
+       clearInterval(this.timer)
     }
 
     render(){
-        return <div>hi</div>
+        return <div>{this.readable_time()}</div>
     }
 
 }
