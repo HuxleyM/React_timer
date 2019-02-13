5a1ba782d36f4d1254529b679c5b842d811f34d1
Huxley_Millard <gerogiamillardccm@gmail.com>
2019-02-13 14:13:40 +0000

    trying to stub time

--- src/tests/TimeFromForm.test.js
+++ src/tests/TimeFromForm.test.js
@@ -7,14 +7,24 @@ import TimeFromForm from '../code/TimerFeature/TimeFromForm';
 describe('TimeFromForm', ()=>{
 
     const dummy_time = '11:00'
-    const formattedTime = new TimeFromForm(dummy_time);
-    var date = new Date();
-    date = date.setHours(11);
-   
 
+    const formattedTime = new TimeFromForm(dummy_time);
+
+    const desiredDate = () => 1550055600449;
+     
+
+
+   
+    /* how can i do this 
+      https://codewithhugo.com/mocking-the-current-date-in-jest-tests/
+      */
 
     it('returns a utc time from form input', ()=>{
-       expect(formattedTime.till).toBe(date)
+       const endTimeStub = jest.fn(()=> desiredDate())
+       formattedTime.endTime = endTimeStub
+       console.log(formattedTime.endTime)
+       console.log(formattedTime)
+       expect(formattedTime.timeTill).toBe(desiredDate())
 
     })
 })
