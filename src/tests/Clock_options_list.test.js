62e7147d7c328342b3bb2696b36a12c258b8da22
Huxley_Millard <gerogiamillardccm@gmail.com>
2019-02-15 14:55:13 +0000

    got jest timers working

--- src/tests/Clock_options_list.test.js
+++ src/tests/Clock_options_list.test.js
@@ -6,9 +6,16 @@ import {shallow, mount} from 'enzyme'
 
 
 describe('clock option list', ()=>{
+    const wrap  = mount( <App />)
+
     it("clicking 'timer' from the menu should change the Apps state mode to 'timer' ", ()=>{
-        const wrap  = mount( <App />)
         wrap.find('#timer_mode').simulate('click')
         expect(wrap.state('mode')).toEqual('timer')
+    })
+
+    it("clicking on timer, then clicking back to clock should change the apps state mode to clock", ()=>{
+        wrap.find('#timer_mode').simulate('click')
+        wrap.find('#clock_mode').simulate('click')
+        expect(wrap.state('mode')).toEqual('clock')
     })
 })
--- src/tests/TimeNow.test.js
+++ src/tests/TimeNow.test.js
@@ -1,35 +1,37 @@
-
-
 import React from 'react'
 import TimeNow from '../code/TimeNowFeature/TimeNow'
+import renderer from 'react-test-renderer';
 import {shallow, mount} from 'enzyme'
 
 describe('clicked on clock', ()=>{
-    // why is this test failing
-    beforeEach(()=>{
-        jest.useFakeTimers();
+ 
+    const wrap = mount( <TimeNow/>) 
+    const currentDate = new Date()  
+    
+    it('will generate correct time_now  ', ()=>{
+        //setup
+        const wrap = shallow( <TimeNow/>)  
+        expect(wrap.instance().generateTime()).toBe(`${currentDate.getHours()}:${currentDate.getMinutes()}:${currentDate.getSeconds()}`)
     })
 
-    const time = new Date();
-    const hour = time.getHours();
-    const mins = time.getMinutes();
-    //lag in code test added a second...
-    const sec = (time.getSeconds());
-
-    const mode = 'clock'
-    const wrap = mount( <TimeNow/>)
-    
-    it('will generate time_now simple component', ()=>{
-        const value = wrap.find('#time_now h2').text()
-        //assertation
-        expect(value).toBe(`${hour}:${mins}:${sec}`)
+    it('should disply this time on the screen', ()=>{
+         const value = wrap.find('#time_now h2').text()
+         expect(value).toBe(`${currentDate.getHours()}:${currentDate.getMinutes()}:${currentDate.getSeconds()}`)
     })
 
     it('should tick', ()=>{
-       //const value = wrap.find('#time_now h2').text()
+        // is need a time wizard
+       jest.useFakeTimers(); 
+       const wrap = mount( <TimeNow/>) 
        expect(setInterval).toHaveBeenCalledTimes(1);
-       expect(setInterval).toHaveBeenLastCalledWith(expect.any(Function), 1000);
-       const sec = (new Date().getSeconds() + 1)
-       expect(value).toBe(`${hour}:${mins}:${sec}`)
+    })
+
+    it('display should change per second', ()=>{
+        // need to do more snap shot testing
+        // const tree = renderer.create(<TimeNow />).toJSON();
+        // expect(tree).toMatchSnapshot();
+        // var secondV=  setTimeout(function() {const secondV = renderer.create(<TimeNow/>).toJSON(); return secondV; }, 1000)
+        // expect(secondV).toMatchSnapshot();
+
     })
 })
