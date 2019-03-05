7f35c95f52753ed0bdeb1862a2ad2b7d74cf42a6
Huxley_Millard <gerogiamillardccm@gmail.com>
2019-03-05 11:50:42 +0000

    The build file

---
+++ build/precache-manifest.856f3b400d66594ce4eb3e2f223844bf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "8cb9b7e0779729f8cbff",
+    "url": "/static/js/main.8cb9b7e0.chunk.js"
+  },
+  {
+    "revision": "6e96b77b8012de2696a9",
+    "url": "/static/js/2.6e96b77b.chunk.js"
+  },
+  {
+    "revision": "8cb9b7e0779729f8cbff",
+    "url": "/static/css/main.e26621db.chunk.css"
+  },
+  {
+    "revision": "baeff66b2e6d7eeb1d63ef097cb6f8cf",
+    "url": "/index.html"
+  }
+];
